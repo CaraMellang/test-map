4a361d2c6782d378435f72e1f9e28b024ca5fbf8
Mellang <dlwpgh23@naver.com>
2021-09-18 21:59:03 +0900

    testMap

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Route } from "react-router";
+import Home from "./components/Home";
+
+const App = () => {
+  return (
+    <div>
+      <h1>ㅎㅇ</h1>
+      <Home />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import KaKaoMap from "./map";
+
+const Home = () => {
+  return (
+    <div>
+      <KaKaoMap />
+    </div>
+  );
+};
+
+export default Home;
